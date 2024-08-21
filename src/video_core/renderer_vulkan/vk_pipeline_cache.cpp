b50e52c2d30c526fb1a6b201e472b038496d5f9d
modmusu <36610531+modmusu@users.noreply.github.com>
2024-08-20 21:53:18 -0400

    .

--- src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
+++ src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
@@ -290,7 +290,7 @@ std::unique_ptr<GraphicsPipeline> PipelineCache::CreateGraphicsPipeline() {
 
         if (hash == 0x8ccd4c7 || hash == 3273382176 || hash == 1253917491 || hash == 3568414570 ||
             hash == 886182625 || hash == 2876255299 || hash == 2153234908 || hash == 0xc0cbc309 ||
-            hash == 0xe0305cef || hash == 2251599991) {
+            hash == 0xe0305cef || hash == 2251599991 || hash == 0x1e042fe95a65db9e) {
             return nullptr;
         }
 
